9ebafcf02df37ab3a6fd5132dbae1c283d435096
Ravi Patel <ravipatel@Ravis-MacBook-Air.local>
2020-05-01 14:13:28 +0530

    update

--- js/common.js
+++ js/common.js
@@ -11,6 +11,8 @@ class homePage {
     }
     allAnimateElement() {
         this.elementFromTop(document.querySelectorAll('.my-skills'),'show-my-skills',90, 'percent'); // as top of element enters bottom of viewport 
+        this.elementFromTop(document.querySelectorAll('.portfolio'),'show-portfolio',70, 'percent'); // as top of element enters bottom of viewport 
+        this.elementFromTop(document.querySelectorAll('.portfolio-cont a'),'show-portfolio-icon',90, 'percent'); // as top of element enters bottom of viewport 
         this.elementFromTop(document.querySelectorAll('.my-skills-row'),'show-my-skills-row',95, 'percent'); // as top of element enters bottom of viewport 
         this.elementFromTop(document.querySelectorAll('.white-section .small-title'),'show-small-title',95, 'percent'); // as top of element enters bottom of viewport 
     }
